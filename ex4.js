c93722cf5b1dc5746895b2ee40fbb025dc96af9e
Kari Cieslak <k882c352@ku.edu>
2017-09-24 21:52:53 -0500

    finished ex. 4

---
+++ ex4.js
@@ -0,0 +1,12 @@
+
+function Backgroundpicker(){
+
+document.body.style.backgroundColor = document.getElementById('background').value;
+
+}
+
+function border_GO(){
+
+document.getElementById('pp').style.borderColor = document.getElementById('border').value;
+
+}
